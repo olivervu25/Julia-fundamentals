0f2f341604c15bddd97a99646f834cdf1bff0bc3
olivervu25 <longvu2000.work@gmail.com>
2023-11-13 17:09:38 +1000

    Monte Carlo Simulation

--- Crash Course by Example/monte_carlo_simulation.jl
+++ Crash Course by Example/monte_carlo_simulation.jl
@@ -11,10 +11,14 @@ N = 10^5
 data = [[rand(), rand()] for _ in 1:N]
 
 indata = filter((x) -> (norm(x) <= 1), data)
-outdata = filter((x) -> (nor(x) > 1), data)
+outdata = filter((x) -> (norm(x) > 1), data)
 
 piApprox = 4*length(indata)/N 
 
 println("Pi Estimate: ", piApprox) 
 
-
+# first.(indata), last.(indata) are used to extract the x and y coordinates of points within the indata
+# c:=blue sets the color of these points to blue 
+# ms=1 and msw=0 set the marker size and marker stroke width, respectively, making points smaller and without an outline 
+scatter(first.(indata),last.(indata), c=:blue, ms=1, msw=0)
+scatter!(first.(outdata),last.(outdata), c=:red, ms=1, msw=0, xlims=(0,1), ylims=(0,1), legend=:none, ratio=:equal)
