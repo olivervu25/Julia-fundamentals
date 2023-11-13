400ae1a29abbf8315d93b68d06f81bff0a8b713f
olivervu25 <longvu2000.work@gmail.com>
2023-11-13 17:34:58 +1000

    Random Walk

---
+++ Crash Course by Example/random_walk.jl
@@ -0,0 +1,31 @@
+using Plots, Random, Measures; pyplot() 
+
+function path(n=5000)
+    x, y = 0.0, 0.0
+    xDat, yDat = [], [] 
+    for _ in 1:n
+        flip = rand(1:4)
+        if flip == 1
+            x += 1
+        elseif flip == 2
+            y += 1
+        elseif flip == 3
+            x -= 1
+        elseif flip == 4
+            y -= 1
+        end 
+        push!(xDat,x)
+        push!(yDat,y)
+
+        println(typeof(flip)) 
+    end 
+    return xDat, yDat
+end 
+
+
+
+default(xlabel = "x", ylabel = "y", xlims=(-150,50), ylims=(-250,50))
+p1 = plot(path(), c=:blue)
+p1 = plot!(path(), c=:red)
+p1 = plot!(path(), c=:green)
+
