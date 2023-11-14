6aa66077ff31710218666dae980d818cab7f57ea
olivervu25 <longvu2000.work@gmail.com>
2023-11-14 14:13:51 +1000

    Predator-prey Lotka Volterra

---
+++ Simulation of Dynamic Models/Predator-Prey Model.jl
@@ -0,0 +1,31 @@
+using Plots, LaTeXStrings; pyplot() 
+
+a, c, d = 2, 1, 5
+
+#The next function in your Julia script defines the behavior of a two-dimensional dynamical system
+next(x,y) = [a*x*(1-x) - x*y, -c*y + d*x*y]
+equibPoint = [(1+c)/d, (d*(a-1)-a*(1+c))/d]
+
+initX = [0.8, 0.05]
+tEnd = 100 
+
+traj = [[] for _ in 1:tEnd]
+traj[1] = initX 
+
+for t in 2:tEnd
+    traj[t] = next(traj[t-1]...)
+
+end 
+
+scatter([traj[1][1]], [traj[1][2]],
+        c = "black", ms=10,
+        label="Intial state")
+
+#scatter! (note the exclamation mark) to add to your existing plot
+plot!(first.(traj), last.(traj),
+        c=:blue, ls=:dash, m=(:dot, 5, Plots.stroke(0)),
+        label = "Model Trajectory")
+
+scatter!([equibPoint[1]], [equibPoint[2]],
+        c= "red", shape=:cross, ms=10, label="Equilibrium point",
+        xlabel=L"X_1", ylabel=L"X_2")
